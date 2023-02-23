5662a576fd678e917ec271509f9684a86bf2c7f5
Noah Fajarda <noahfajarda@gmail.com>
2023-02-23 10:49:37 -0800

    from HW demo

--- README.md
+++
@@ -1,1 +0,0 @@
-# 20-React-Portfolio
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [theme, setTheme] = useState("light");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div
+            className={`${
+                theme === "dark" ? "bg-black" : "bg-light"
+            } w-screen h-screen flex items-center justify-center p-12 relative`}
+        >
+            <div
+                className={`w-full h-full border 
+                ${theme === "dark" ? "border-white" : "border-black"}
+            `}
+            ></div>
+            <div
+                className={`absolute bottom-12 left-3 flex flex-col space-y-2
+                ${theme === "dark" ? "text-white" : "text-black"}`}
+            >
+                <p
+                    onClick={() =>
+                        setTheme((prevTheme) =>
+                            prevTheme === "light" ? "dark" : "light"
+                        )
+                    }
+                    className="flex flex-col space-y-2 items-center"
+                >
+                    <span
+                        className={`h-5 w-4 flex-shrink-0 border border-white 
+                        ${theme === "dark" ? "border-white" : "border-black"}
+                        ${theme === "light" && "bg-white"}`}
+                    ></span>
+                    <span style={{ "writing-mode": "vertical-rl" }}>LIGHT</span>
+                </p>
+                <p
+                    onClick={() =>
+                        setTheme((prevTheme) =>
+                            prevTheme === "light" ? "dark" : "light"
+                        )
+                    }
+                    className="flex flex-col space-y-2 items-center"
+                >
+                    <span
+                        className={`h-5 w-4 flex-shrink-0 border border-white 
+                        ${theme === "dark" ? "border-white" : "border-black"}
+                        ${theme === "dark" && "bg-white"}`}
+                    ></span>
+                    <span style={{ "writing-mode": "vertical-rl" }}>DARK</span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
